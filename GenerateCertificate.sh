8d8a9671096bc437af1b665b4b453869604ea6f9
Vitor Gomes <vitor.shade@gmail.com>
2017-01-24 22:30:34 -0200

    -Add bash script to generate a self-signed certificate

---
+++ GenerateCertificate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+openssl genrsa -des3 -out server.key 2048 && 
+openssl rsa -in server.key -out server.key && 
+openssl req -sha256 -new -key server.key -out server.csr -subj '/CN=localhost' &&
+openssl x509 -req -sha256 -days 365 -in server.csr -signkey server.key -out server.crt
