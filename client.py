54ac4434536548311ca912df64df346717695a08
Vitor Gomes <vitor.shade@gmail.com>
2017-01-25 00:02:44 -0200

    -Add basic client-server authentication code

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import ssl
 import socket
+import json
 
 
 class Client:
@@ -10,18 +11,49 @@ class Client:
         self.context.verify_mode = ssl.CERT_REQUIRED
         self.context.check_hostname = True
         self.context.load_verify_locations(cert_file_name)
-        # For testing purposes
-        self.context.check_hostname = False
+        self.socket = None
 
     def connect(self, address, port, hostname):
-        conn = self.context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=hostname)
-        conn.connect((address, port))
+        self.socket = self.context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=hostname)
+        self.socket.connect((address, port))
 
     def login(self, username, password):
         print('Login executed')
+        self.socket.sendall(self.build_login_message(username, password))
+        print("Waiting for response...")
+        if self.handle_response():
+            print('Login Success!')
+        else:
+            print('Login Failed!')
+
+    def handle_response(self):
+        try:
+            data = self.socket.recv(1024)
+            while True:
+                if data:
+                    json_message = json.loads(data.decode('utf-8'))
+                    if self.validate(json_message):
+                        return self.get_status(json_message)
+                else:
+                    break
+                data = self.socket.recv(1024)
+        finally:
+            self.socket.shutdown(socket.SHUT_RDWR)
+            self.socket.close()
+
+    def build_login_message(self, username, password):
+        login_message = '{"message_type":"LOGIN_MESSAGE", "username":"' + username + '", "password":"' + password + '"}'
+        print("Login request is: " + login_message)
+        return login_message.encode('utf-8')
+
+    def validate(self, json_message):
+        return json_message['message_type'] == 'LOGIN_RESPONSE'
+
+    def get_status(self, json_message):
+        return json_message['status'] == 'SUCCESS'
 
 
 if __name__ == '__main__':
     client = Client('server.crt')
-    client.connect('localhost', 9000, 'MyServer')
+    client.connect('localhost', 9003, 'localhost')
     client.login("myuser", "mypassword")
--- sever.py
+++ sever.py
@@ -1,5 +1,6 @@
 import ssl
 import socket
+import json
 
 
 class Server:
@@ -7,6 +8,7 @@ class Server:
         print("Server instantiated")
         self.context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
         self.context.load_cert_chain(certfile=cert_filename, keyfile=key_filename)
+        self.server_socket = None
 
     def start(self, address, port):
         bind_socket = socket.socket()
@@ -19,20 +21,56 @@ class Server:
         print("Server listening")
         while True:
             new_socket, from_address = bind_socket.accept()
-            conn_stream = self.context.wrap_socket(new_socket, server_side=True)
+            self.server_socket = self.context.wrap_socket(new_socket, server_side=True)
             try:
-                self.authenticate(conn_stream)
+                self.check_messages()
+            except ConnectionResetError as e:
+                print("Caught exception: " + str(e))
+                pass
             finally:
-                conn_stream.shutdown(socket.SHUT_RDWR)
-                conn_stream.close()
+                self.close_connection()
 
-    def authenticate(self, conn_stream):
-        data = conn_stream.recv(1024)
-        print("Received data")
-        print(data)
-        while data:
-            data = conn_stream.recv(1024)
+    def close_connection(self):
+        try:
+            self.server_socket.shutdown(socket.SHUT_RDWR)
+            self.server_socket.close()
+        except OSError:
+            pass
+
+    def check_messages(self):
+        data = self.server_socket.recv(1024)
+        while True:
+            if data:
+                print("Received message")
+                self.authenticate(data)
+            else:
+                break
+            data = self.server_socket.recv(1024)
+
+    def authenticate(self, data):
+        json_message = json.loads(data.decode('utf-8'))
+        if self.validate(json_message):
+            if self.valid_credentials(json_message):
+                self.send_login_message("SUCCESS")
+            else:
+                self.send_login_message("FAILED")
+
+    def valid_credentials(self, json_message):
+        # Actual user and password validation should go here
+        return json_message['username'] == 'myuser' and json_message['password'] == 'mypassword'
+
+    def send_login_message(self, status):
+        login_response = '{"message_type":"LOGIN_RESPONSE", "status":"' + status + '"}'
+        print("Login response is: " + login_response)
+        self.server_socket.sendall(login_response.encode('utf-8'))
+
+    def validate(self, json_message):
+        if json_message['message_type'] == "LOGIN_MESSAGE":
+            print("Valid message received: " + str(json_message))
+            return True
+        return False
+
 
 if __name__ == '__main__':
     server = Server("server.crt", "server.key")
-    server.start('localhost', 9000)
+    server.start('localhost', 9003)
